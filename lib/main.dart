87d620256a267fa54a827c6a6085880af5cf0cc7
hesham <heshamhamdan51@gmail.com>
2024-03-11 17:38:27 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
