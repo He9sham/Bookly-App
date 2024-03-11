4edad83abfa29b6e155bab3da807d759240012bb
hesham <heshamhamdan51@gmail.com>
2024-03-11 17:46:00 +0200

    add the get package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -8,6 +9,8 @@ class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const GetMaterialApp(
+      
+    );
   }
 }
