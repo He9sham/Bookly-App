ef4322893e221a8383a2cf6ce66dfd350fab7b87
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-07-23 03:20:32 +0300

    add new splash screen

--- lib/book_app.dart
+++ lib/book_app.dart
@@ -1,39 +1,19 @@
 import 'package:bookly_app/const.dart';
-import 'package:bookly_app/core/Services/server_locator.dart';
 import 'package:bookly_app/core/utils/app_routers.dart';
-import 'package:bookly_app/features/home/data/repo/home_repo_impl.dart';
-import 'package:bookly_app/features/home/logic/feature_books_list/feature_books_list_cubit.dart';
-import 'package:bookly_app/features/home/logic/newset_books_list/newset_books_list_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => FeatureBooksListCubit(
-            getIt.get<HomeRepoImpl>(),
-          )..fetchFeatureBooks(),
-        ),
-        BlocProvider(
-          create: (context) => NewsetBooksListCubit(
-            getIt.get<HomeRepoImpl>(),
-          )..fetchNewsetBooks(),
-        ),
-      ],
-      child: MaterialApp.router(
-        routerConfig: AppRouter.router,
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: kprimarycolor,
-          textTheme:
-              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-        ),
-        debugShowCheckedModeBanner: false,
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kprimarycolor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
